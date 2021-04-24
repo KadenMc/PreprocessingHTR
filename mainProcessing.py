beee55f55704609a9ef96cde320d360eb3cf08ce
KadenMc <mckeen.kaden.school@hotmail.com>
2021-04-24 13:31:18 -0400

    Reduce size of images in README.md for faster loading.

--- mainProcessing.py
+++ mainProcessing.py
@@ -10,6 +10,25 @@ import connectedComponentsProcessing
 import lineClustering
 import lineAnalysis
 
+
+
+def preprocess(image_path, save_path):
+
+    # Load the image
+    img = cv2.imread(image_path)
+
+    # Create ImageAnalysis class
+    image_anlysis = ImageAnalysis(img)
+
+    # Get word images
+    return image_anlysis.get_words(save_path=save_path)
+
+def img_resize(img, scale=0.5):
+    width = int(img.shape[1] * scale)
+    height = int(img.shape[0] * scale)
+    dsize = (width, height)
+    return cv2.resize(img, dsize)
+
 class ImageAnalysis:
     
     def __init__(self, img):
@@ -65,18 +84,18 @@ class ImageAnalysis:
 
     # Saves pre-processing images
     def save_images(self, save_path):
-        cv2.imwrite(save_path + "/0img.jpg", self.img)
-        cv2.imwrite(save_path + "/1bordered.jpg", self.bordered)
-        cv2.imwrite(save_path + "/2circles_removed.jpg", self.circles_removed)
-        cv2.imwrite(save_path + "/3lines_removed.jpg", self.lines_removed)
-        cv2.imwrite(save_path + "/4gray.jpg", self.gray)
-        cv2.imwrite(save_path + "/5blurred.jpg", self.blurred)
-        cv2.imwrite(save_path + "/6edges.jpg", self.edges)
-        cv2.imwrite(save_path + "/7dilated.jpg", self.dilated)
-        cv2.imwrite(save_path + "/8components.jpg", self.components_img)
-        cv2.imwrite(save_path + "/9components_filtered.jpg", self.components.filtered)
-        cv2.imwrite(save_path + "/10components_borders.jpg", self.components.borders)
-        cv2.imwrite(save_path + "/11lines_img.jpg", self.line_img)
+        cv2.imwrite(save_path + "/0img.jpg", img_resize(self.img))
+        cv2.imwrite(save_path + "/1bordered.jpg", img_resize(self.bordered))
+        cv2.imwrite(save_path + "/2circles_removed.jpg", img_resize(self.circles_removed))
+        cv2.imwrite(save_path + "/3lines_removed.jpg", img_resize(self.lines_removed))
+        cv2.imwrite(save_path + "/4gray.jpg", img_resize(self.gray))
+        cv2.imwrite(save_path + "/5blurred.jpg", img_resize(self.blurred))
+        cv2.imwrite(save_path + "/6edges.jpg", img_resize(self.edges))
+        cv2.imwrite(save_path + "/7dilated.jpg", img_resize(self.dilated))
+        cv2.imwrite(save_path + "/8components.jpg", img_resize(self.components_img))
+        cv2.imwrite(save_path + "/9components_filtered.jpg", img_resize(self.components.filtered))
+        cv2.imwrite(save_path + "/10components_borders.jpg", img_resize(self.components.borders))
+        cv2.imwrite(save_path + "/11lines_img.jpg", img_resize(self.line_img))
 
         for i, l in enumerate(self.line_imgs):
             cv2.imwrite(save_path + "/line{}.jpg".format(i), self.line_imgs[i])
@@ -86,17 +105,6 @@ class ImageAnalysis:
                 cv2.imwrite(save_path + "/word{}_{}.jpg".format(i, j), w[0])
                 cv2.imwrite(save_path + "/word{}_{}_backup.jpg".format(i, j), w[1])
 
-
-def preprocess(image_path, save_path):
-
-    # Load the image
-    img = cv2.imread(image_path)
-
-    # Create ImageAnalysis class
-    image_anlysis = ImageAnalysis(img)
-
-    # Get word images
-    return image_anlysis.get_words(save_path=save_path)
 
 
 if __name__ == "__main__":
